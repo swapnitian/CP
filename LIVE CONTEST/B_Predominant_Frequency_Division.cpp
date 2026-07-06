88046be35e9e13c9e72f4511ac0e6826b47007cc
swapnitian <sjayant097.btech2024@ee.nitrr.ac.in>
2026-07-06 21:41:47 +0530

    Contest DIV-2

---
+++ LIVE CONTEST/B_Predominant_Frequency_Division.cpp
@@ -0,0 +1,172 @@
+/*
+    ███████╗ ██╗    ██╗  █████╗  ██████╗  ███╗   ██╗ ██╗ ████████╗
+    ██╔════╝ ██║    ██║ ██╔══██╗ ██╔══██╗ ████╗  ██║ ██║ ╚══██╔══╝
+    ███████╗ ██║ █╗ ██║ ███████║ ██████╔╝ ██╔██╗ ██║ ██║    ██║
+    ╚════██║ ██║███╗██║ ██╔══██║ ██╔═══╝  ██║╚██╗██║ ██║    ██║
+    ███████║ ╚███╔███╔╝ ██║  ██║ ██║      ██║ ╚████║ ██║    ██║
+    ╚══════╝  ╚══╝╚══╝  ╚═╝  ╚═╝ ╚═╝      ╚═╝  ╚═══╝ ╚═╝    ╚═╝
+
+            Code. Debug. Optimize. Repeat.
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// ===================== DEFINES =====================
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+
+#define pb push_back
+#define ff first
+#define ss second
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+// ===================== FAST IO =====================
+void fastIO() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+}
+
+// ===================== MODULAR EXPO =====================
+ll modPow(ll a, ll b, ll mod = MOD) {
+    ll res = 1;
+    a %= mod;
+
+    while (b) {
+        if (b & 1)
+            res = (res * a) % mod;
+
+        a = (a * a) % mod;
+        b >>= 1;
+    }
+    return res;
+}
+
+// ===================== MODULAR INVERSE =====================
+ll modInverse(ll a, ll mod = MOD) {
+    return modPow(a, mod - 2, mod);
+}
+
+// ===================== SIEVE =====================
+const int MAX_SIEVE = 1e6 + 5;
+
+vector<bool> isPrime(MAX_SIEVE, true);
+vector<int> primes;
+
+void sieve() {
+    isPrime[0] = isPrime[1] = false;
+
+    for (int i = 2; i * i < MAX_SIEVE; i++) {
+        if (isPrime[i]) {
+            for (int j = i * i; j < MAX_SIEVE; j += i)
+                isPrime[j] = false;
+        }
+    }
+
+    for (int i = 2; i < MAX_SIEVE; i++) {
+        if (isPrime[i])
+            primes.pb(i);
+    }
+}
+
+// ===================== FACTORIALS =====================
+const int MAXN = 1e6 + 5;
+
+vector<ll> fact(MAXN);
+vector<ll> invFact(MAXN);
+
+void precomputeFactorials() {
+    fact[0] = 1;
+
+    for (int i = 1; i < MAXN; i++)
+        fact[i] = (fact[i - 1] * i) % MOD;
+
+    invFact[MAXN - 1] = modInverse(fact[MAXN - 1]);
+
+    for (int i = MAXN - 2; i >= 0; i--)
+        invFact[i] = (invFact[i + 1] * (i + 1)) % MOD;
+}
+
+// ===================== NCR =====================
+ll nCr(ll n, ll r) {
+    if (r < 0 || r > n)
+        return 0;
+
+    return (((fact[n] * invFact[r]) % MOD)
+            * invFact[n - r]) % MOD;
+}
+
+// ===================== NPR =====================
+ll nPr(ll n, ll r) {
+    if (r < 0 || r > n)
+        return 0;
+
+    return (fact[n] * invFact[n - r]) % MOD;
+}
+
+// ===================== GCD / LCM =====================
+ll gcdll(ll a, ll b) {
+    return __gcd(a, b);
+}
+
+ll lcmll(ll a, ll b) {
+    return (a / gcdll(a, b)) * b;
+}
+
+// ===================== SOLVE =====================
+void solve() {
+    int n;
+    cin >> n;
+
+    vector<int> arr(n);
+
+    for(int i = 0; i < n; i++) cin >> arr[i];
+
+    vector<int> pref1(n+1);
+    vector<int> pref2(n+1);
+
+    for(int i = 0; i < n; i++){
+        if(arr[i] == 1){
+            pref1[i+1] = pref1[i] + 1; 
+        }else if(arr[i] == 2 || arr[i] == 3){
+            pref1[i+1] = pref1[i] - 1;
+        }
+
+        if(arr[i] == 3){
+            pref2[i+1] = pref2[i] -1;
+        }else if(arr[i] == 1 || arr[i] == 2){
+            pref2[i+1] = pref2[i] + 1;
+        }
+    }
+    vector<int> suff(n+1, -1e9); 
+
+    for(int i = n-1; i > 0; i--){
+        suff[i] = max(suff[i+1], pref2[i]);
+    }
+
+    for(int i = 1; i < n-1; i++){
+        if(pref1[i] >= 0 && suff[i+1] >= pref2[i]){
+            cout << "YES\n";
+            return;
+        }
+    }
+    cout << "NO\n";
+}
+
+int main() {
+    fastIO();
+
+    int T;
+    cin >> T;
+    while (T--) {
+        solve();
+    }
+
+    return 0;
+}
